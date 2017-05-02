03916f0751bc5d55179645144324e5fc785b6c31
mamazu <maxo.pech@gmail.com>
2017-05-02 09:47:29 +0200

    Fixed CSS bug

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Grapher</title>
 	<script type="text/javascript" src="js/lib/p5.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
-	<link rel="stylesheet" type="text/css" href="script/main.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 </body>
