188a7574ba862a99a85c966e6cbd95a6d8060e92
mamazu <maxo.pech@gmail.com>
2017-05-02 15:47:56 +0200

    Starting to draw arrows

--- js/Edge.js
+++ js/Edge.js
@@ -6,10 +6,21 @@ function Edge(from, to){
 }
 
 Edge.prototype.show = function() {
-	ellipse(this.to.pos.x, this.to.pos.y, 20);
+	function drawingTheTrianlge(anchor){
+		fill(0);
+		// Moving the triangle
+		push();
+		translate(anchor.x, anchor.y)
+		triangle(0, 0, 10, 10, 20, 0);
+		pop();
+	}
+
+	// Drawing the text
 	if(text != undefined){
 		textAlign(CENTER, BOTTOM);
 		text(this.text, this.textPos.x, this.textPos.y);
 	}
+	//Drawing the line and the direction
 	line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
+	drawingTheTrianlge(this.to.pos);
 };
