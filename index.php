f35809a0ee5241ed3a4d1b7c23c9516409f554dd
mamazu <maxo.pech@gmail.com>
2017-05-02 09:44:40 +0200

    Cleaned up and implemented css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Grapher</title>
 	<script type="text/javascript" src="js/lib/p5.js"></script>
 	<script type="text/javascript" src="js/script.js"></script>
+	<link rel="stylesheet" type="text/css" href="script/main.css">
 </head>
 <body>
 </body>
