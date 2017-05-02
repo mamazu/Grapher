78f64f379c4f5c84c3755627353ee3ccaf007aa3
mamazu <maxo.pech@gmail.com>
2017-05-02 10:15:04 +0200

    Added a show function for showable objects to simplify iterating

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 nodes = []
+edges = []
+
+function shower(element, index, array){
+	element.show();
+}
 
 function setup(){
 	createCanvas(window.innerWidth, window.innerHeight);
 	nodes.push(new Node(200, 200, "This is sparta"));
+	nodes.push(new Node(400, 400, "This is brocolli"));
+	edges.push(new Edge(nodes[0], nodes[1]));
 }
 
 function draw(){
 	background(240);
-	for (var i = nodes.length - 1; i >= 0; i--) {
-		nodes[i].show();
-	}
+	nodes.forEach(shower)
+	edges.forEach(shower);
 }
 
 function windowRezised(){
