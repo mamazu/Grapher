d3becd0d3c6d8259b7fe6a6226879bfe2414f613
mamazu <maxo.pech@gmail.com>
2017-05-02 17:37:34 +0200

    Added priotity constants

--- js/Node.js
+++ js/Node.js
@@ -1,11 +1,18 @@
-function Node(x, y, text){
+const CRUCIAL = 3;
+const NICE_TO_HAVE = 2;
+const OPTIONAL = 1;
+
+function Node(x, y, text, priority) {
 	this.pos = createVector(x, y);
 	this.text = text;
-	this.priority = 1;
+	if (priority == undefined)
+		this.priority = NICE_TO_HAVE;
+	else
+		this.priority = priority;
 }
 
 Node.prototype.show = function() {
-	rectColor = lerpColor(color(255,0,0), color(0,255,0), this.priority);
+	rectColor = lerpColor(color(255, 0, 0), color(0, 255, 0), this.priority);
 	fill(rectColor);
 	rect(this.pos.x, this.pos.y, textWidth(text), -16);
 	textAlign(LEFT, BOTTOM);
