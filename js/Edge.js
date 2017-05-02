8b0cd0ae317e8e393731430ef9e2eeac62a808d1
mamazu <maxo.pech@gmail.com>
2017-05-02 10:34:38 +0200

    Added text to edges

--- js/Edge.js
+++ js/Edge.js
@@ -1,8 +1,14 @@
 function Edge(from, to){
 	this.from = from;
 	this.to = to;
+	this.text = "hello";
+	this.textPos = createVector(from.pos.x + (to.pos.x - from.pos.x) / 2, from.pos.y + (to.pos.y - from.pos.y) / 2)
 }
 
 Edge.prototype.show = function() {
+	if(text != undefined){
+		textAlign(CENTER, BOTTOM);
+		text(this.text, this.textPos.x, this.textPos.y);
+	}
 	line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
 };
