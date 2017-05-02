915e8cda3cdef77fb8de5072c3a93a3eda7fc62f
mamazu <maxo.pech@gmail.com>
2017-05-02 15:36:01 +0200

    Added color to priority

--- js/Node.js
+++ js/Node.js
@@ -1,10 +1,12 @@
 function Node(x, y, text){
 	this.pos = createVector(x, y);
 	this.text = text;
+	this.priority = 1;
 }
 
 Node.prototype.show = function() {
-	fill(255);
+	rectColor = lerpColor(color(255,0,0), color(0,255,0), this.priority);
+	fill(rectColor);
 	rect(this.pos.x, this.pos.y, textWidth(text), -16);
 	textAlign(LEFT, BOTTOM);
 	fill(0);
