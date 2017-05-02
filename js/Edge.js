53955be1eda7489ec5ba8ba8360f725821a74640
mamazu <maxo.pech@gmail.com>
2017-05-02 20:36:56 +0200

    Added default text and a difference vector

--- js/Edge.js
+++ js/Edge.js
@@ -1,40 +1,43 @@
-function Edge(from, to) {
+function Edge(from, to, text) {
 	this.from = from;
 	this.to = to;
-	this.text = "hello";
-	this.textPos = createVector(from.pos.x + (to.pos.x - from.pos.x) / 2, from.pos.y + (to.pos.y - from.pos.y) / 2)
+	this.text = text;
+	this.distance = createVector(to.pos.x - from.pos.x, to.pos.y - from.pos.y)
+	this.textPos = createVector(from.pos.x + this.distance.x / 2, from.pos.y + this.distance.y / 2)
 }
 
 Edge.prototype.show = function() {
+	console.log(degrees(this.distance.heading()), this.distance, this.text)
+
 	function drawingTheTrianlge(anchor, angle) {
 		fill(0);
 		// Moving the triangle
 		push();
-		translate(anchor.x - 10, anchor.y - 10);
+		translate(anchor.x, anchor.y);
 		rotate(angle - HALF_PI);
 		triangle(-10, 0, 0, 10, 10, 0);
 		pop();
 	}
 
 	// Drawing the text
-	if (text != undefined) {
+	if (this.text != undefined) {
 		textAlign(CENTER, BOTTOM);
 		push();
 		translate(this.textPos.x, this.textPos.y)
-		rotate(this.textPos.heading());
+		rotate(this.distance.heading);
 		text(this.text, 0, 0);
 		pop();
 	}
 	//Drawing the line and the direction
 	line(this.from.pos.x, this.from.pos.y, this.to.pos.x, this.to.pos.y);
-	drawingTheTrianlge(this.to.pos, this.textPos.heading());
+	drawingTheTrianlge(this.to.pos, this.distance.heading());
 }
 
 Edge.prototype.adapt = function() {
 	if (this.to.priority <= this.from.priority)
 		return;
 	this.from.priority = this.to.priority;
-	edges.foreach(function(e) {
+	edges.forEach(function(e) {
 		if (e.to == this.from)
 			e.adapt();
 	})
