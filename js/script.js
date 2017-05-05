fe9660f11c07e85c5dc02d475bd0e435ee2f4aab
mamazu <maxo.pech@gmail.com>
2017-05-05 23:46:22 +0200

    Added view features like zooming and dragging

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 nodes = []
 edges = []
+view = {
+	'x': 0,
+	'y': 0,
+	'scale': 1,
+}
 
+//User defined
 function shower(element) {
 	element.show();
 }
 
+
+// P5 Functions
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
 	textSize(16);
@@ -17,20 +25,39 @@ function setup() {
 }
 
 function draw() {
+	translate(view.x, view.y);
+	scale(view.scale);
 	background(240);
 	edges.forEach(function(element) {
 		element.adapt();
 		shower(element);
 	});
 	nodes.forEach(shower);
+	grid();
 }
 
+// Mouse event
 function mouseClicked() {
 	nodes.forEach(function(node) {
 		node.click(mouseX, mouseY);
 	});
 }
 
+function mouseMoved() {}
+
+function mouseDragged(evt) {
+	view.x += evt.movementX;
+	view.y += evt.movementY;
+}
+
+function mouseWheel(evt) {
+	if (evt.delta > 0 && view.scale > 0.25)
+		view.scale -= 0.125;
+	else if (evt.delta < 0)
+		view.scale += 0.125
+}
+
+// Window actions
 function windowRezised() {
 	resizeCanvas(window.innerWidth, window.innerHeight);
 }
